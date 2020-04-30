4241565cecdeb75aba3af78473af700cbfc343ab
Melissa LeBlanc-Williams <melissa@adafruit.com>
2020-04-30 11:52:41 -0700

    Filled out some RTD ToDos

--- docs/conf.py
+++ docs/conf.py
@@ -17,11 +17,10 @@ extensions = [
     "sphinx.ext.todo",
 ]
 
-# TODO: Please Read!
 # Uncomment the below if you use native CircuitPython modules such as
 # digitalio, micropython and busio. List the modules you use. Without it, the
 # autodoc module docs will fail to generate with a warning.
-# autodoc_mock_imports = ["digitalio", "busio"]
+autodoc_mock_imports = ["busio"]
 
 
 intersphinx_mapping = {
